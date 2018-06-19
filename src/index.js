71a6b19a6f7be2d2abc0e579a696d618e2d6deef
Akriti <sehrawat@usc.edu>
2018-06-19 13:10:05 -0700

    feat(website): Connecting to Graphcool

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import gql from 'graphql-tag';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import getAllPosts from './server/queries/getAllPosts';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  uri: `${process.env.REACT_APP_GRAPHCOOL_SIMPLE_URI}`,
+});
+
+client
+  .query({ query: getAllPosts })
+  .then(results => console.log('testing on server', results.data));
+
+const ApolloWrappedApp = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
+
+ReactDOM.render(<ApolloWrappedApp />, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/server/queries/getAllPosts.js
@@ -0,0 +1,10 @@
+import gql from 'graphql-tag';
+
+export default gql`
+  {
+    allPosts {
+      id,
+      title
+    }
+  }
+`;
