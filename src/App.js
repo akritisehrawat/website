ebbcd860c1e2e22c0ce858048a7e0d8c328b9fa0
Akriti <sehrawat@usc.edu>
2018-06-19 11:38:56 -0700

    feat(website): Adding routing and styled-components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import styled, { injectGlobal } from 'styled-components';
+
+injectGlobal`
+  html, body {
+    width: 100vw;
+    height: 100vh;
+
+    border: 0;
+    margin: 0;
+  }
+`;
+
+const a = () => <div>a</div>;
+const b = () => <div>b</div>;
 
 class App extends Component {
   render() {
     return (
-      <div>
-
-      </div>
+      <Router>
+        <Fragment>
+          <Route path="/a" component={a} />
+          <Route path="/b" component={b} />
+        </Fragment>
+      </Router>
     );
   }
 }
